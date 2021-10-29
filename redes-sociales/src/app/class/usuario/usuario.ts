79080315fa22d78e0f8f0c2a7018c26583dedf96
Klau <klaudiusmiskinis@gmail.com>
2021-10-29 18:43:10 +0200

    Terminando GETTERs y SETTERs

--- redes-sociales/src/app/class/usuario/usuario.ts
+++ redes-sociales/src/app/class/usuario/usuario.ts
@@ -7,6 +7,7 @@ export class Usuario {
     password: string;
     descripcion: string;
     
+    /* CONSTRUCTOR */
     constructor(nombre: string, apellidos: string, correo: string, edad: number, avatar: File, password: string, descripcion: string){
         this.nombre = nombre;
         this.apellidos = apellidos;
@@ -17,4 +18,62 @@ export class Usuario {
         this.descripcion = descripcion;
     }
 
+    /* GETTERs */
+    getNombre(): string {
+        return this.nombre;
+    }
+
+    getApellidos(): string {
+        return this.apellidos;
+    }
+    
+    getCorreo(): string {
+        return this.correo;
+    }
+
+    getEdad(): number {
+        return this.edad;
+    }
+
+    getAvatar(): File {
+        return this.avatar;
+    }
+
+    getPassword(): string {
+        return this.password;
+    }
+
+    getDescripcion(): string {
+        return this.descripcion;
+    }
+
+
+    /* SETTERs */
+    setNombre(nombre: string): void {
+        this.nombre = nombre;
+    }
+
+    setApellidos(apellidos: string): void {
+        this.apellidos = apellidos;
+    }
+
+    setCorreo(correo: string): void {
+        this.correo = correo;
+    }
+
+    setEdad(edad: number): void {
+        this.edad = edad;
+    }
+
+    setAvatar(avatar: File): void {
+        this.avatar = avatar;
+    }
+
+    setPassword(password: string): void {
+        this.password = password;
+    }
+
+    setDescripcion(descripcion): void {
+        this.descripcion = descripcion
+    }
 }
