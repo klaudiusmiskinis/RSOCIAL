9234db6bb3049087be333f26f44854d6c2726336
Klau <klaudiusmiskinis@gmail.com>
2021-11-02 18:47:10 +0100

    Importando clase Usuario

--- redes-sociales/src/app/app.component.ts
+++ redes-sociales/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Usuario } from './class/usuario/usuario';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'redes-sociales';
 }
