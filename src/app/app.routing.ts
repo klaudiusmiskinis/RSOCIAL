f656d6263f889d2a591e5f0abe17ffee9bcaab63
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-04 12:17:59 +0100

    Cambio de comillas, rutas y redireccionamientos. Tambien se llama a los distintos componentes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-    { path: "", component: AppComponent, pathMatch: "full" },
-    { path: "login", component: LoginComponent, pathMatch: "full" },
-    { path: "register", component: RegisterComponent, pathMatch: "full" },
-
-    // REDIRECCIÓN AL HOME SI NO SON LAS RUTAS DE ARRIBA.
+    { path: '',   redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent, pathMatch: 'full' },
+    { path: 'register', component: RegisterComponent, pathMatch: 'full' },
     { path: '**', redirectTo: '' }
 ];
 
