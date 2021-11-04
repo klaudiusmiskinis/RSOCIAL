b97b1fe18703b9b37b0011e2efcf82e1e87d649d
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-04 12:17:34 +0100

    Insertando APP_BASE_HREF para funcionamiento de providers y routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@
 import { Routing } from './app.routing';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
 /* IMPORTANDO COMPONENTES */
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BackgroundComponent } from './background/background.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,9 +20,10 @@ import { BackgroundComponent } from './background/background.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     Routing
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue : '/' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
