5c3d75b52bf03190f2e6e39014cc4b3d0abc08fe
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-04 12:44:19 +0100

    Rehaciendo la estructura HTML del login y login.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
-=======
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
->>>>>>> c150ba7a644c5bcfd34ee22873aea7b051e282e4
 
 @Component({
   selector: 'app-login',
@@ -12,7 +6,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['../app.component.css']
 })
 
-<<<<<<< HEAD
 export class LoginComponent {
   email: string;
   password: string;
@@ -24,42 +17,4 @@ export class LoginComponent {
     console.log(this.password);
   }
 }
-=======
-export class LoginComponent implements OnInit {
-    loginForm: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-    ) { }
-    ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
-
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    }
-
-    // convenience getter for easy access to form fields
-    get f() { return this.loginForm.controls; }
-
-    onSubmit() {
-        this.submitted = true;
-
-        // reset alerts on submit
-
-        // stop here if form is invalid
-        if (this.loginForm.invalid) {
-            return;
-        }
-
-        this.loading = true;
-    } 
-}
->>>>>>> c150ba7a644c5bcfd34ee22873aea7b051e282e4
