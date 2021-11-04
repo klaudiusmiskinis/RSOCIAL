bbf01d4be714ab76e45cce1c0a31b3d2d1c052c3
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-04 10:30:26 +0100

    Cambiando archivo de estilo para no duplicar codigo

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['../app.component.css']
 })
 export class RegisterComponent implements OnInit {
 
