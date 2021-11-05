ad8a950c7023782afbb2dae75af3c3c0e8df302b
Klau <klaudiusmiskinis@gmail.com>
2021-11-05 20:01:25 +0100

    Eliminando import de ControlContainer no utilizado

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ControlContainer, FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
