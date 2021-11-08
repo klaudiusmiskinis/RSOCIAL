5b0deca56681d86bdf0700124740f8a94f2c0a44
Klau <klaudiusmiskinis@gmail.com>
2021-11-08 20:11:13 +0100

    Cambios significativos

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,24 +7,71 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 
 export class RegisterComponent {
-  username: string;
-  email: string;
-  password: string;
   registerForm: FormGroup;
+  validado: boolean = false;
+  error = {
+    texto: '',
+    clase: ''
+  }
 
-  constructor() {};
+  constructor(private formBuilder: FormBuilder) {};
 
-  crearFormulario() {
-    this.registerForm = new FormGroup({
-      username: new FormControl(),
-      email: new FormControl(),
-      password: new FormControl()
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        email: ['', Validators.required],
+        password: ['', Validators.required],
+        passwordRepetir: ['', Validators.required]
     });
   }
 
-  register() {
-    console.log(this.username);
-    console.log(this.email);
-    console.log(this.password);
-  };
-};
+  validacionUsername() {
+    let valor = this.registerForm.value.username
+    if (valor.length > 3) {
+      this.error.texto = 'El username es válido';
+      this.error.clase = 'mt-2 p-2 bg-success text-black rounded';
+    } else {
+      this.error.texto = 'El username es demasiado corto';
+      this.error.clase = 'mt-2 p-2 bg-danger text-black rounded';
+    }
+  }
+
+  validacionEmail() {
+    let valor = this.registerForm.value.email
+    if (valor.length > 3) {
+      this.error.texto = 'El email es válido';
+      this.error.clase = 'mt-2 p-2 bg-success text-black rounded'
+    } else {
+      this.error.texto = 'El email es demasiado corto';
+      this.error.clase = 'mt-2 p-2 bg-danger text-black rounded'
+    }
+  }
+
+  validacionPassword() {
+    let valor = this.registerForm.value.password
+    if(valor.length > 3) {
+      this.error.texto = 'La contraseña es válida';
+      this.error.clase = 'mt-2 p-2 bg-success text-black rounded'
+    } else {
+      this.error.texto = 'La contraseña es demasiado corto';
+      this.error.clase = 'mt-2 p-2 bg-danger text-black rounded'
+    }
+  }
+
+  validacionPasswordRepetir() {
+    let valor = this.registerForm.value.passwordRepetir
+    let password = this.registerForm.value.password
+    if(valor.length > 3) {
+      if (valor === password) {
+        this.error.texto = 'Las contraseñas coinciden';
+        this.error.clase = 'mt-2 p-2 bg-success text-black rounded'
+      } else {
+        this.error.texto = 'Las contraseñas no coinciden';
+        this.error.clase = 'mt-2 p-2 bg-danger text-black rounded'
+      }
+    } else {
+      this.error.texto = 'La contraseña repetida es demasiado corta';
+      this.error.clase = 'mt-2 p-2 bg-danger text-black rounded'
+    }
+  }
+}
