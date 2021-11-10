8d82aa4a164c5dfdfe3598caecdb49b8c632abdd
Klau <klaudiusmiskinis@gmail.com>
2021-11-10 19:13:00 +0100

    Añadiendo nuevo componente Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BackgroundComponent } from './background/background.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
