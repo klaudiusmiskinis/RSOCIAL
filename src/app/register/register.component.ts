7000819818440c099b3e4d3dca41820dc850bc8d
Klau <klaudiusmiskinis@gmail.com>
2021-11-10 19:04:52 +0100

    Añadiendo metodos encargados de comprobacion de email y username. Tambien se ha optimizado los errores en pantalla

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -12,7 +12,7 @@ export class RegisterComponent {
   error = {
     texto: '',
     clase: ''
-  }
+  };
 
   constructor(private formBuilder: FormBuilder) {};
 
@@ -25,53 +25,59 @@ export class RegisterComponent {
     });
   }
 
+  comprobarEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  comprobarUsername(username){
+    var usernameRegex = /^[a-zA-Z0-9]+$/;
+    return usernameRegex.test(username);
+  }
+
+  llamarError(tipo, textoError) {
+    if (tipo) {
+      this.error.texto = textoError;
+      this.error.clase = 'mt-2 p-2 bg-success text-black rounded';
+    } else {
+      this.error.texto = textoError;
+      this.error.clase = 'mt-2 p-2 bg-danger text-black rounded';
+    }
+  }
+
   validacionUsername() {
     let valor = this.registerForm.value.username
     if (valor.length > 3) {
-      this.error.texto = 'El username es válido';
-      this.error.clase = 'mt-2 p-2 bg-success text-black rounded';
+      if(this.comprobarUsername(valor)) {
+        this.llamarError(true, 'El username es válido');
+      } else {
+        this.llamarError(false, 'El username es inválido');
+      }
     } else {
-      this.error.texto = 'El username es demasiado corto';
-      this.error.clase = 'mt-2 p-2 bg-danger text-black rounded';
+      this.llamarError(false, 'El username es demasiado corto');
     }
   }
 
   validacionEmail() {
     let valor = this.registerForm.value.email
-    if (valor.length > 3) {
-      this.error.texto = 'El email es válido';
-      this.error.clase = 'mt-2 p-2 bg-success text-black rounded'
-    } else {
-      this.error.texto = 'El email es demasiado corto';
-      this.error.clase = 'mt-2 p-2 bg-danger text-black rounded'
-    }
+      if(this.comprobarEmail(valor)) {
+        this.llamarError(true, 'El email es válido');
+      } else {
+        this.llamarError(false, 'El email es inválido');
+      }
   }
 
   validacionPassword() {
-    let valor = this.registerForm.value.password
-    if(valor.length > 3) {
-      this.error.texto = 'La contraseña es válida';
-      this.error.clase = 'mt-2 p-2 bg-success text-black rounded'
-    } else {
-      this.error.texto = 'La contraseña es demasiado corto';
-      this.error.clase = 'mt-2 p-2 bg-danger text-black rounded'
-    }
-  }
-
-  validacionPasswordRepetir() {
     let valor = this.registerForm.value.passwordRepetir
     let password = this.registerForm.value.password
     if(valor.length > 3) {
       if (valor === password) {
-        this.error.texto = 'Las contraseñas coinciden';
-        this.error.clase = 'mt-2 p-2 bg-success text-black rounded'
+        this.llamarError(true, 'Las contraseñas coinciden');
       } else {
-        this.error.texto = 'Las contraseñas no coinciden';
-        this.error.clase = 'mt-2 p-2 bg-danger text-black rounded'
+        this.llamarError(false, 'Las contraseñas no coinciden');
       }
     } else {
-      this.error.texto = 'La contraseña repetida es demasiado corta';
-      this.error.clase = 'mt-2 p-2 bg-danger text-black rounded'
+      this.llamarError(false, 'Las contraseñas no coinciden');
     }
   }
 }
