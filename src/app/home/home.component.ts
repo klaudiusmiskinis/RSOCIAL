b944eb0bf8e1607cdb200c22a451d99dd57263ef
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-11 13:11:17 +0100

    Añadiendo funcionamiento del HOME

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UsuariosService } from '../services/usuarios.service';
 
 @Component({
   selector: 'home',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../app.component.css']
 })
 export class HomeComponent implements OnInit {
+  public router;
+  public route;
+  public usuarios;
+  public logged;
 
-  constructor() { }
+  constructor(Router: Router, ActivatedRoute: ActivatedRoute, UsuariosService: UsuariosService) {
+    this.router = Router;
+    this.route = ActivatedRoute
+    this.usuarios = UsuariosService;
+    this.logged = this.getParametros();
+  }
 
   ngOnInit(): void {
+    if (!this.logged) return this.router.navigate(['login'])
+    this.logged = this.usuarios.usuarios.filter(usuario => usuario.correo === this.logged);
+    console.log(this.logged);
+  }
+
+  getParametros() {
+   return this.route.params._value.usuario
   }
 
 }
