894ce6422c5da845300db69d9369f2cc1ba41efc
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-12 15:59:35 +0100

    Generando componente background-gradiente

---
+++ src/app/background-gradiente/background-gradiente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-gradiente',
+  templateUrl: './background-gradiente.component.html',
+  styleUrls: ['./background-gradiente.component.css']
+})
+export class BackgroundGradienteComponent {
+  constructor() { }
+}
