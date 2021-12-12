fcc6ab197d2d1b80645e64758515171186f9804d
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-12 14:07:40 +0100

    Minimizando ts del background

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.css']
 })
-export class BackgroundComponent implements OnInit {
+export class BackgroundComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
