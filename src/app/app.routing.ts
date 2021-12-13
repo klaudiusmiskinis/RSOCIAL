2f9fcb7f92b59b6e289b43c8a8b7843062fb48d7
Klau <klaudiusmiskinis@gmail.com>
2021-12-13 19:25:22 +0100

    Cambiando ruta y componente

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { PoliticaPrivacidadComponent } from './politica-privacidad/politica-privacidad.component';
+import { PrivacidadComponent } from './privacidad/privacidad.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
@@ -9,7 +9,7 @@ const routes: Routes = [
     { path: 'login', component: LoginComponent, pathMatch: 'full' },
     { path: 'register', component: RegisterComponent, pathMatch: 'full' },
     { path: 'home', component: HomeComponent, pathMatch: 'full' },
-    { path: 'politica-de-privacidad', component: PoliticaPrivacidadComponent, pathMatch: 'full'},
+    { path: 'politica', component: PrivacidadComponent, pathMatch: 'full'},
     { path: '**', redirectTo: '' }
 ];
 
