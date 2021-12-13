f7c711bf1f4a3952a9149eaba3f96039b2d01962
Klau <klaudiusmiskinis@gmail.com>
2021-12-13 19:07:12 +0100

    Componente de politica de privacidad

---
+++ src/app/politica-privacidad/politica-privacidad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-politica-privacidad',
+  templateUrl: './politica-privacidad.component.html',
+  styleUrls: ['../app.component.css']
+})
+export class PoliticaPrivacidadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
