9dca38f973e6f4e44909cb66ea2de0df971fe41c
Klau <klaudiusmiskinis@gmail.com>
2021-12-14 20:07:59 +0100

    Añadiendo routing a politica, perfil, logout y login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,19 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { PerfilComponent } from './perfil/perfil.component';
 import { PrivacidadComponent } from './privacidad/privacidad.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-    { path: '',   redirectTo: '/login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent, pathMatch: 'full' },
-    { path: 'register', component: RegisterComponent, pathMatch: 'full' },
-    { path: 'home', component: HomeComponent, pathMatch: 'full' },
     { path: 'politica', component: PrivacidadComponent, pathMatch: 'full'},
+    { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+    { path: 'perfil', component: PerfilComponent, pathMatch: 'full'},
+    { path: 'logout', component: LogoutComponent, pathMatch: 'full' },
+    { path: 'login', component: LoginComponent, pathMatch: 'full' },
+    { path: 'home', component: HomeComponent, pathMatch: 'full' },
+    { path: '',   redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: '' }
 ];
 
