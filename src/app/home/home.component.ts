8b799864745f2e9c715cd7b56e2461cc444daa2f
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-16 00:23:46 +0100

    Creando funcionalidad de Swiper y validaciones para correcto funcionamiento

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from '../class/usuario';
 import { UsuariosService } from '../services/usuarios.service';
-import SwiperCore from 'swiper';
+import { SwiperOptions, Thumbs } from 'swiper';
 
 
 @Component({
@@ -12,23 +13,50 @@ import SwiperCore from 'swiper';
 
 
 export class HomeComponent implements OnInit {
-  public router;
-  public route;
+  public router: Router;
+  public route: ActivatedRoute;
   public usuarios;
-  public logged;
+  public logged: Usuario;
+  public amigos;
+  public disabled: boolean = false;
+  public show: boolean = true;
+  public type: string = 'component';
+  public config: SwiperOptions = {
+    a11y: { enabled: true },
+    direction: 'horizontal',
+    slidesPerView: 1,
+    keyboard: true,
+    mousewheel: true,
+    scrollbar: false,
+    navigation: false,
+    pagination: false,
+    autoplay: {
+      delay: 3000,
+    }
+  };
 
   constructor(Router: Router, ActivatedRoute: ActivatedRoute, UsuariosService: UsuariosService) {
     this.router = Router;
     this.route = ActivatedRoute
     this.usuarios = UsuariosService;
-    this.logged = localStorage.getItem('user');
+    this.amigos = []
+    this.logged = this.usuarios.findUsuarioByEmail(localStorage.getItem('user'))[0];
   }
 
-  ngOnInit(): void {
-    if (!this.logged) return this.router.navigate(['login']);
-    this.logged = this.usuarios.usuarios.filter(usuario => usuario.correo === this.logged);
-    this.logged = this.logged[0];
+  ngOnInit() {
+    if (!this.logged) this.router.navigate(['login']);
+    else {
+      this.logged.amigos.forEach(amigo => {
+        this.amigos.push(this.usuarios.findUsuarioByEmail(amigo)[0]);
+      })
+    }
+    console.log(this.amigos)
   }
 
+  public onSwiperEvent(event: string): void {
+  }
+
+  public onIndexChange(index: number): void {
+  }
 
 }
