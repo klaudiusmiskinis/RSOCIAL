6b59e86d4ee528a309ef8755dcdf706e74b8a80a
Klau <klaudiusmiskinis@gmail.com>
2021-12-15 19:22:27 +0100

    Añadiendo splide y inicializandolo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UsuariosService } from '../services/usuarios.service';
+import { Splide } from '@splidejs/splide';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['../app.component.css']
 })
+
 export class HomeComponent implements OnInit {
   public router;
   public route;
@@ -18,15 +20,14 @@ export class HomeComponent implements OnInit {
     this.route = ActivatedRoute
     this.usuarios = UsuariosService;
     this.logged = localStorage.getItem('user');
+    new Splide('.splide').mount();
   }
 
   ngOnInit(): void {
     if (!this.logged) return this.router.navigate(['login']);
     this.logged = this.usuarios.usuarios.filter(usuario => usuario.correo === this.logged);
-    this.logged = this.logged[0]
+    this.logged = this.logged[0];
   }
 
-  getParametros() {
-   return this.route.params._value.usuario
-  }
+
 }
