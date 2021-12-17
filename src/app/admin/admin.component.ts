82215fedef9e30266f0f42dd131721634f6ab35f
Klau <klaudiusmiskinis@gmail.com>
2021-12-17 19:50:15 +0100

    Creando atributos

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuariosService } from '../services/usuarios.service';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-admin',
@@ -9,13 +10,23 @@ import { UsuariosService } from '../services/usuarios.service';
 
 export class AdminComponent implements OnInit {
   public usuarios;
-
+  public columnas: ColDef[] = [
+    {headerName: 'Nombre', field: 'nombre'},
+    {headerName: 'Apellidos', field: 'apellidos'},
+    {headerName: 'Correo', field: 'correo'},
+    {headerName: 'Edad', field: 'edad'},
+    {headerName: 'Avatar', field: 'avatar'},
+    {headerName: 'Password', field: 'password'},
+    {headerName: 'Descripción', field: 'descripcion'},
+    {headerName: 'Rol', field: 'rol'},
+    {headerName: 'Amigos', field: 'amigos'},
+  ]
+  
   constructor(usuariosService: UsuariosService) {
     this.usuarios = usuariosService.getUsuarios();
   }
 
   ngOnInit(): void {
-    
-  }
 
+  }
 }
