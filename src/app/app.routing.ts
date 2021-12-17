f86bf2b37835ef548bf0fd9f98bfa5ee68fc71ba
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-17 12:51:07 +0100

    Añadiendo componente admin y creando ruta con su respectivo componente

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,4 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PerfilComponent } from './perfil/perfil.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
     { path: 'politica', component: PrivacidadComponent, pathMatch: 'full'},
     { path: 'register', component: RegisterComponent, pathMatch: 'full' },
     { path: 'perfil', component: PerfilComponent, pathMatch: 'full'},
+    { path: 'admin', component: AdminComponent, pathMatch: 'full' },
     { path: 'login', component: LoginComponent, pathMatch: 'full' },
     { path: 'home', component: HomeComponent, pathMatch: 'full' },
     { path: '',   redirectTo: '/login', pathMatch: 'full' },
