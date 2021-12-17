77f7778d3423d5163d748b3a9a2335cf5d350aaa
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-17 01:05:01 +0100

    Funcionalidad completa del perfil y formulario de cambio de datos

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsuariosService } from '../services/usuarios.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
@@ -9,23 +10,39 @@ import { UsuariosService } from '../services/usuarios.service';
 })
 
 export class PerfilComponent implements OnInit {
+  public actualizarDatosForm: FormGroup;
   public router;
   public logged;
   private usuarios;
 
-  constructor(Router: Router, usuariosService: UsuariosService) {
+  constructor(Router: Router, usuariosService: UsuariosService, private formBuilder: FormBuilder) {
+    this.formBuilder = formBuilder;
     this.router = Router;
     this.usuarios = usuariosService;
     this.logged = this.usuarios.findUsuarioByEmail(localStorage.getItem('user'))[0]
+    this.actualizarDatosForm = this.formBuilder.group({
+      nombre: [this.logged.nombre, Validators.required],
+      apellidos: [this.logged.apellidos, Validators.required],
+      edad: [this.logged.edad, Validators.required],
+      descripcion: [this.logged.descripcion, Validators.required]
+    });
   }
 
   ngOnInit(): void {
     if (!this.logged) return this.router.navigate(['login']);
-  
   }
 
-  actualizar() {
-    console.log()
+  actualizarDatos() {
+    let actualizar = false;
+    let nombre = this.actualizarDatosForm.value.nombre
+    let apellidos = this.actualizarDatosForm.value.apellidos
+    let edad = this.actualizarDatosForm.value.edad
+    let descripcion = this.actualizarDatosForm.value.descripcion
+    if (this.logged.nombre != nombre) this.logged.nombre = nombre, actualizar = true;
+    if (this.logged.apellidos != apellidos) this.logged.apellidos = apellidos, actualizar = true; 
+    if (this.logged.edad != edad) this.logged.edad = edad,  actualizar = true; 
+    if (this.logged.descripcion != descripcion) this.logged.descripcion = descripcion, actualizar = true; 
+    if (actualizar) this.usuarios.actualizarUsuario(this.logged)
   }
 
 }
