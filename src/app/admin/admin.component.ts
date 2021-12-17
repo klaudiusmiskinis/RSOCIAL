54608c051528db5298d80068b056bc5b9bb0b141
Klau <klaudiusmiskinis@gmail.com>
2021-12-17 17:38:30 +0100

    Añadiendo funcionalidad al atributo usuarios

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from '../services/usuarios.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['../app.component.css']
 })
-export class AdminComponent implements OnInit {
 
-  constructor() { }
+export class AdminComponent implements OnInit {
+  public usuarios;
+
+  constructor(usuariosService: UsuariosService) {
+    this.usuarios = usuariosService.getUsuarios();
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
