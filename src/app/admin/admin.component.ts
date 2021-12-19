36fd5e7680aa83cc32a9ccd50de0236e1220a9c4
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-19 17:58:16 +0100

    Cambiando atributos, inicializandolos en el constructor, creando metodos para un correcto funcionamiento de ag-grid

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,46 +1,50 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { Usuario } from '../class/usuario';
 import { UsuariosService } from '../services/usuarios.service';
 
 @Component({
   selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['../app.component.css']
+  templateUrl: './admin.component.html'
 })
 
 export class AdminComponent {
   /* Métodos */
   public gridApi;
   public gridColumnApi;
+  public defaultColDef
+  public rowSelection;
+  public seleccionado;
   public usuarios;
   public servicio;
-  public rowSelection = 'single';
-  public seleccionado;
-  public columnas = [
-    {headerName: 'Nombre', field: 'nombre', sortable: true},
-    {headerName: 'Apellidos', field: 'apellidos', sortable: true},
-    {headerName: 'Correo', field: 'correo', sortable: true},
-    {headerName: 'Edad', field: 'edad', sortable: true},
-    {headerName: 'Avatar', field: 'avatar', sortable: true},
-    {headerName: 'Descripción', field: 'descripcion', sortable: true},
-    {headerName: 'Rol', field: 'rol', sortable: true},
-    {headerName: 'Amigos', field: 'amigos', sortable: true}
-  ]
-  public defaultColDef = {
-    editable: true,
-    resizable: true,
-  }
+  public columnas;
 
+  /* Constructor */
   constructor(usuariosService: UsuariosService) {
     this.servicio = usuariosService;
     this.usuarios = this.servicio.getUsuarios();
+    this.rowSelection = 'single';
+    this.columnas = [
+      {headerName: 'Nombre', field: 'nombre', sortable: true},
+      {headerName: 'Apellidos', field: 'apellidos', sortable: true},
+      {headerName: 'Correo', field: 'correo', sortable: true},
+      {headerName: 'Edad', field: 'edad', sortable: true},
+      {headerName: 'Avatar', field: 'avatar', sortable: true},
+      {headerName: 'Descripción', field: 'descripcion', sortable: true},
+      {headerName: 'Rol', field: 'rol', sortable: true},
+      {headerName: 'Amigos', field: 'amigos', sortable: true}
+    ]
+    this.defaultColDef = {
+      editable: true,
+      resizable: true,
+    }
   }
 
   /* Métodos */
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    this.gridApi.sizeColumnsToFit();
+    this.columnaMovida(event);
   }
+  
   
   campoCambiando(event) {
     const selectedRows = this.gridApi.getSelectedRows();
@@ -53,7 +57,7 @@ export class AdminComponent {
     this.usuarios = this.servicio.getUsuarios();
   }
 
-  onSelectionChanged(event) {
+  seleccionarFila(event): void {
     const selectedRows = this.gridApi.getSelectedRows();
     this.seleccionado = {
       nombre: selectedRows[0].nombre + ' ' + selectedRows[0].apellidos,
@@ -61,16 +65,20 @@ export class AdminComponent {
     }
   }
 
-  eliminarRow() {
+  eliminarFila(): void {
     if (this.seleccionado.correo) {
       this.servicio.eliminarUsuario(this.servicio.findUsuarioByEmail(this.seleccionado.correo)[0]);
       this.usuarios = this.servicio.getUsuarios();
     }
   }
 
-  tablaCargada(params) {
+  tablaCargada(params): void {
     this.gridApi = params.api;
     this.gridApi.setDomLayout('autoHeight');
+    this.gridApi.sizeColumnsToFit();
+  }
+
+  columnaMovida(event): void {
     this.gridApi.sizeColumnsToFit();
   }
 }
