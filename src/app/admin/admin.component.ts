fa9a2b93129436ccefa5870fad1e8d3f5fe956bd
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-19 02:46:42 +0100

    Añadiendo diferentes funcionalidades

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Usuario } from '../class/usuario';
 import { UsuariosService } from '../services/usuarios.service';
 
 @Component({
@@ -7,12 +8,14 @@ import { UsuariosService } from '../services/usuarios.service';
   styleUrls: ['../app.component.css']
 })
 
-export class AdminComponent implements OnInit {
+export class AdminComponent {
+  /* Métodos */
   public gridApi;
   public gridColumnApi;
   public usuarios;
   public servicio;
   public rowSelection = 'single';
+  public seleccionado;
   public columnas = [
     {headerName: 'Nombre', field: 'nombre', sortable: true},
     {headerName: 'Apellidos', field: 'apellidos', sortable: true},
@@ -23,7 +26,6 @@ export class AdminComponent implements OnInit {
     {headerName: 'Rol', field: 'rol', sortable: true},
     {headerName: 'Amigos', field: 'amigos', sortable: true}
   ]
-
   public defaultColDef = {
     editable: true,
     resizable: true,
@@ -34,10 +36,7 @@ export class AdminComponent implements OnInit {
     this.usuarios = this.servicio.getUsuarios();
   }
 
-  ngOnInit(): void {
-    
-  }
-
+  /* Métodos */
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.gridApi.sizeColumnsToFit();
@@ -45,7 +44,28 @@ export class AdminComponent implements OnInit {
   
   campoCambiando(event) {
     const selectedRows = this.gridApi.getSelectedRows();
-    const user = this.servicio.findUsuarioByEmail(selectedRows[0].correo)
+    this.seleccionado = {
+      nombre: selectedRows[0].nombre + ' ' + selectedRows[0].apellidos,
+      correo: selectedRows[0].correo
+    }
+    const usuarioCambiar: Usuario = event.data;
+    this.servicio.actualizarUsuario(usuarioCambiar);
+    this.usuarios = this.servicio.getUsuarios();
+  }
+
+  onSelectionChanged(event) {
+    const selectedRows = this.gridApi.getSelectedRows();
+    this.seleccionado = {
+      nombre: selectedRows[0].nombre + ' ' + selectedRows[0].apellidos,
+      correo: selectedRows[0].correo
+    }
+  }
+
+  eliminarRow() {
+    if (this.seleccionado.correo) {
+      this.servicio.eliminarUsuario(this.servicio.findUsuarioByEmail(this.seleccionado.correo)[0]);
+      this.usuarios = this.servicio.getUsuarios();
+    }
   }
 
   tablaCargada(params) {
