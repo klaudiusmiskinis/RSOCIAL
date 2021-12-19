fb81d95d88043c334c2c6a03ee2a4e66bcea6a3a
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-19 18:26:27 +0100

    Añadiendo ruta de ChatComponent

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { ChatComponent } from './chat/chat.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PerfilComponent } from './perfil/perfil.component';
@@ -13,6 +14,7 @@ const routes: Routes = [
     { path: 'admin', component: AdminComponent, pathMatch: 'full' },
     { path: 'login', component: LoginComponent, pathMatch: 'full' },
     { path: 'home', component: HomeComponent, pathMatch: 'full' },
+    { path: 'chat', component: ChatComponent, pathMatch: 'full' },
     { path: '',   redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: '' }
 ];
