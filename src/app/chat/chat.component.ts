caf2635c5d2d976da02e63739963b0d9e5dd309e
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-19 18:34:00 +0100

    Añadiendo router

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuariosService } from '../services/usuarios.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
@@ -9,15 +10,17 @@ export class ChatComponent implements OnInit {
   public usuarios;
   public servicio;
   public logged;
+  public router;
 
-  constructor(UsuariosService: UsuariosService) { 
-    this.servicio = UsuariosService;
+  constructor(usuariosService: UsuariosService, router: Router) { 
+    this.servicio = usuariosService;
+    this.router = router;
     this.usuarios = this.servicio.getUsuarios();
     this.logged = this.servicio.findUsuarioByEmail(localStorage.getItem('user'))[0];
   }
 
   ngOnInit(): void {
-    
+    if (!this.logged) this.router.navigate(['login']);
   }
 
 }
