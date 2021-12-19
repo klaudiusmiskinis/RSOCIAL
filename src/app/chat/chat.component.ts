a2705ae7bdc39588e3cbbe4e911ab6699544b0ef
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-19 23:49:57 +0100

    Creando funcionamientos y metodos

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -11,16 +11,25 @@ export class ChatComponent implements OnInit {
   public servicio;
   public logged;
   public router;
+  public amigos;
 
   constructor(usuariosService: UsuariosService, router: Router) { 
     this.servicio = usuariosService;
     this.router = router;
     this.usuarios = this.servicio.getUsuarios();
     this.logged = this.servicio.findUsuarioByEmail(localStorage.getItem('user'))[0];
+    this.getAmigos();
   }
 
   ngOnInit(): void {
     if (!this.logged) this.router.navigate(['login']);
+  }
+
+  getAmigos() {
+    console.log(this.logged)
+    this.logged.amigos.forEach(amigo => {
+      this.amigos.push(this.servicio.findUsuarioByEmail(amigo)[0]);
+    })
   }
 
 }
