d397b1c995c1b3f5e328cb7d12bb933b0308e682
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-20 10:36:44 +0100

    Añadiendo comentarios

--- src/app/privacidad/privacidad.component.ts
+++ src/app/privacidad/privacidad.component.ts
@@ -5,15 +5,19 @@ import { Router } from '@angular/router';
   selector: 'app-privacidad',
   templateUrl: './privacidad.component.html'
 })
+
 export class PrivacidadComponent implements OnInit {
+  /* Atributos */
   public logged;
   public router;
-
+  
+  /* Constructor */
   constructor(Router: Router) {
     this.router = Router;
     this.logged = localStorage.getItem('user');
   }
 
+  /* Métodos */
   ngOnInit(): void {
     if (!this.logged) return this.router.navigate(['login']);
   }
