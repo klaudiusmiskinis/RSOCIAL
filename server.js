0c1de85a4547dcc441c052c5196979e3ecc8a689
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-20 12:31:12 +0100

    Cambiando server

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
-app.use(express.static(__dirname+'/dist'));
-
-app.listen(process.env.PORT||8080);
-
-
-//Path Location Strategy
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.use(express.static(`${__dirname}/front-end/dist/`));
+app.get('*', (req, res) => {
+  res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
-
 console.log('Console Listening'); 
