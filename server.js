9b0236eacb7629b30fd50c9d4815865e7bb11c03
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-20 11:31:30 +0100

    Cambiando rutas

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/rsocial'));
+app.use(express.static('./dist/rsocial'));
 app.get('*', (req, res) => {
-    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile('index.html', {root: 'dist/rsocial/'})
 })
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
