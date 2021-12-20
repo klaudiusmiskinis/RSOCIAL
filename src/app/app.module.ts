444b838bc9783b5ab502bc8f708cc5c46d2d5926
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-20 10:34:38 +0100

    Estructuración visual del código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,39 @@
 /* IMPORTANDO LIBRERIAS DE ANGULAR */
-import { Routing } from './app.routing';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { APP_BASE_HREF } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SwiperModule } from 'ngx-swiper-wrapper';
-import { AgGridModule } from 'ag-grid-angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF }       from '@angular/common';
+import { BrowserModule }       from '@angular/platform-browser';
+import { SwiperModule }        from 'ngx-swiper-wrapper';
+import { AgGridModule }        from 'ag-grid-angular';
+import { FormsModule }         from '@angular/forms';
+import { NgModule }            from '@angular/core';
+import { Routing }             from './app.routing';
+
 
 /* IMPORTANDO COMPONENTES */
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { BackgroundComponent } from './background/background.component';
-import { HomeComponent } from './home/home.component';
-import { PrivacidadComponent } from './privacidad/privacidad.component';
-import { PerfilComponent } from './perfil/perfil.component';
 import { PanelLateralPerfilComponent } from './panel-lateral-perfil/panel-lateral-perfil.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AdminComponent } from './admin/admin.component';
+import { BackgroundComponent }         from './background/background.component';
+import { PrivacidadComponent }         from './privacidad/privacidad.component';
+import { RegisterComponent }           from './register/register.component';
+import { PerfilComponent }             from './perfil/perfil.component';
+import { NavbarComponent }             from './navbar/navbar.component';
+import { LoginComponent }              from './login/login.component';
+import { AdminComponent }              from './admin/admin.component';
+import { HomeComponent }               from './home/home.component';
+import { AppComponent }                from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    BackgroundComponent,
-    HomeComponent,
-    PrivacidadComponent,
-    PerfilComponent,
     PanelLateralPerfilComponent,
+    BackgroundComponent,
+    PrivacidadComponent,
+    RegisterComponent,
+    PerfilComponent,
     NavbarComponent,
+    LoginComponent,
     AdminComponent,
+    HomeComponent,
+    AppComponent,
   ],
   imports: [
     AgGridModule.withComponents([]),
