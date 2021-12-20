0b2c58ec80554194bda8739f479fbe4fe997a492
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-20 11:26:19 +0100

    Añadiendo GET

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/rsocial'));
+app.get('*', (req, res) => {
+    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+})
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/rsocial/index.html'));});
